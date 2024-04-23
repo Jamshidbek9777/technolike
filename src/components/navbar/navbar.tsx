831c566f115aeb83c1929d9c4a1932e3dbeedc77
Jamshidbek <jamshidbekrasulovuz@gmail.com>
2024-04-23 22:25:39 +0500

    mobile-drawer

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { Flex } from "antd";
+import { Drawer, Flex } from "antd";
 import Logo from "../../assets/logo.jpg";
 import { Box, Container } from "..";
 import styled from "styled-components";
@@ -12,18 +12,29 @@ const Navbar = () => {
      const [active, setActive] = useState(1);
      const navigate = useNavigate();
      const location = useLocation();
+     const [open, setOpen] = useState(false);
 
      const handleChange = (path: any, index: any) => {
           navigate(path);
           active;
           setActive(index);
      };
+     const showDrawer = () => {
+          setOpen(true);
+     };
+
+     const onClose = () => {
+          setOpen(false);
+     };
 
      return (
           <Container>
                <NavbarContainer justify="space-between">
                     <Hamburger $align="center">
-                         <HiOutlineBars3CenterLeft fontSize={30} />
+                         <HiOutlineBars3CenterLeft
+                              fontSize={30}
+                              onClick={showDrawer}
+                         />
                     </Hamburger>
 
                     <Box $align="center" $gap="10px">
@@ -32,7 +43,7 @@ const Navbar = () => {
                               src={Logo}
                               alt="logo"
                          />
-                         Technolike
+                         <h3>TECHNOLIKE</h3>
                     </Box>
                     <NavItems $align="center" $gap="25px">
                          <NavItem
@@ -65,6 +76,42 @@ const Navbar = () => {
                          <CiSearch style={{ fontSize: "25px" }} />
                     </Box>
                </NavbarContainer>
+               <Drawer
+                    width={260}
+                    title="TECHNOLIKE"
+                    placement={"left"}
+                    closable={false}
+                    onClose={onClose}
+                    open={open}
+                    key={"left"}>
+                    <Flex vertical gap={10}>
+                         <NavItem
+                              onClick={() => handleChange("/", 1)}
+                              active={location.pathname === "/"}>
+                              Asosiy
+                         </NavItem>
+                         <NavItem
+                              onClick={() => handleChange("/news", 2)}
+                              active={location.pathname === "/news"}>
+                              Yangliliklar
+                         </NavItem>
+                         <NavItem
+                              onClick={() => handleChange("/about", 3)}
+                              active={location.pathname === "/about"}>
+                              Biz haqimizda
+                         </NavItem>
+                         <NavItem
+                              onClick={() => handleChange("/youtube", 4)}
+                              active={location.pathname === "/youtube"}>
+                              You Tube
+                         </NavItem>
+                         <NavItem
+                              onClick={() => handleChange("/contact", 5)}
+                              active={location.pathname === "/contact"}>
+                              Bog'lanish
+                         </NavItem>
+                    </Flex>
+               </Drawer>
           </Container>
      );
 };
@@ -73,6 +120,7 @@ const NavbarContainer = styled(Flex)`
      height: 60px;
      cursor: pointer;
      padding: 10px 0;
+     /* user-select: none; */
 `;
 
 const NavItem = styled.div<any>`
